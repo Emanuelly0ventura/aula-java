3aee3984f9120d7004eae4196fd43fb6fa11dd2c
manu <emanuelly.ventura.n@gmail.com>
2025-03-13 12:12:17 -0300

    objetos,atributos e variaveis

---
+++ src/Livro.java
@@ -0,0 +1,20 @@
+import javax.xml.transform.sax.SAXResult;
+
+public class Livro {
+    //atributo tipo namoAtributo;
+
+    String titulo;
+
+    String autor;
+
+    double preco;
+
+    String editor;
+
+    int paginas;
+
+    String resumo;
+
+    String tipoCapa;
+
+}
---
+++ src/Testelivro.java
@@ -0,0 +1,15 @@
+public class Testelivro {
+    public static void main(String[] args) {
+        //criar um onjeto -> instanciar um objeto
+    // sintaxe: tipo nome do objeto = new Tipo();
+
+        Livro meulivro = new Livro();
+        meulivro.titulo = "ArloFich";
+        meulivro.paginas = 400;
+        meulivro.preco = 35;
+        meulivro.resumo = "um menino se mudou para sua cidade natal...";
+
+                Livro LrivroDaNana = new Livro();
+
+    }
+}
